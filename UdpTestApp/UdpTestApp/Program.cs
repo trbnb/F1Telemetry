bb4481efb2e457aae8cfe9b9ce30cf2bf6fec27b
Thorben Buchta <thorben.buchta@in-tech.com>
2017-11-20 18:06:22 +0100

    fix(recorder): fix data recorder

--- UdpTestApp/UdpTestApp/Program.cs
+++ UdpTestApp/UdpTestApp/Program.cs
@@ -8,16 +8,18 @@ using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Utils;
 
 namespace UdpTestApp
 {
     class Program
     {
-        private const int Packages = 100;
+        private const int PackageSize = 1289;
+        private const int Packages = 3000;
 
         public static void Main(string[] args)
         {
-            var client = new UdpClient(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20777));
+            var client = new UdpClient(new IPEndPoint(IPAddress.Any, 20777));
             Task.Factory.StartNew(() => WaitForPackages(client));
 
             Console.ReadLine();
@@ -27,14 +29,34 @@ namespace UdpTestApp
         {
             var packageStore = new List<byte[]>(Packages);
 
-            for(var i = 0; i < Packages; i++)
+            for (var i = 0; i < Packages; i++)
             {
                 var result = await client.ReceiveAsync();
                 packageStore.Add(result.Buffer);
+                Console.WriteLine($"Value Nr. {i + 1}");
             }
 
-            var strings = packageStore.Select(e => BitConverter.ToString(e));
-            File.WriteAllLines(@"%userprofile%/desktop/testdata.txt", strings);
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/F1Telemetry/testdata1.txt";
+
+            using (var writer = File.Create(path))
+            {
+                foreach (var byteArray in packageStore)
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+            }
+
+            PrintBytes(path);
+        }
+
+        private static void PrintBytes(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            
+            var isValid = bytes.Length % PackageSize == 0;
+            var dataSets = bytes.Split(PackageSize);
+
+            Console.WriteLine($"Sets: {dataSets.Length}; isValid: {isValid}");
         }
     }
 }
